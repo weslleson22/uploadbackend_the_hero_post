43fe3f285dedfeddd85f13f309664c41f6a99c88
Wesleson Souza Silva <31991735+weslleson22@users.noreply.github.com>
2020-05-17 20:20:09 -0300

    importações pendentes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require('express');
 const cors = require ('cors');
 const {errors} = require('celebrate');
@@ -8,6 +10,10 @@ app.use(cors({}));
 app.use(express.json());
 app.use(routes);
 app.use(errors());
+
+
+app.listen(process.env.PORT || 3000);
+
 
 
 
