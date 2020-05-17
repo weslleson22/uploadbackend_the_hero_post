234926e9968eef6381b1ec982e2f857e4dd39baa
Wesleson Souza Silva <31991735+weslleson22@users.noreply.github.com>
2020-05-17 16:02:16 -0300

    Add files via upload

---
+++ knexfile.js
@@ -0,0 +1,62 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './src/database/db_sqlite3'
+    },
+    migrations: {
+      directory: './src/database/migrations'
+    
+    },
+    useNullAsDefault: true,
+
+  },
+  test: {
+    client: 'sqlite3',
+    connection: {
+      filename: './src/database/test_sqlite3'
+    },
+    migrations: {
+      directory: './src/database/migrations'
+    
+    },
+    useNullAsDefault: true,
+
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
