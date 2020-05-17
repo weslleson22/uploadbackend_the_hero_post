7ec7e48139ba12d35073437d0b675f8554862c75
Wesleson Souza Silva <31991735+weslleson22@users.noreply.github.com>
2020-05-17 20:25:01 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
+
+
+app.listen(process.env.PORT || 3333);
+
 require("dotenv").config();
 
 const express = require('express');
 const cors = require ('cors');
 const {errors} = require('celebrate');
-const routes = require('./routes');
-
-const app = express();
-app.use(cors({}));
-app.use(express.json());
-app.use(routes);
+ app.use(routes);
 app.use(errors());
 
 
 app.listen(process.env.PORT || 3000);
-
-
 
 
 
