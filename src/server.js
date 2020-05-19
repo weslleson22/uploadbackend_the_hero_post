22d624b5ccfeb38f689110f5a46a35ec9922dd2e
Wesleson Souza Silva <31991735+weslleson22@users.noreply.github.com>
2020-05-18 22:35:37 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,15 +1,8 @@
 require ("dotenv").config();
 
-const path = require('path')
 const app = require('./app');
 
 
 
-const PORT = process.env.PORT || 5000
+app.listen(process.env.PORT || 3333);
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('src', path.join(__dirname, 'src'))
-  .set('src engine', 'ejs')
-  .get('/', (req, res) => res.render('server'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
