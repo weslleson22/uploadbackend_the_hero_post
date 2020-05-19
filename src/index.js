c513217c57a16f5ee9e76267dd8e97f61571c0ec
Wesleson Souza Silva <31991735+weslleson22@users.noreply.github.com>
2020-05-19 12:35:04 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
 
 
 
-require("dotenv").config();
+const express = require('express')
+const cors = require('cors')
 
-const express = require('express');
-const cors = require ('cors');
-const {errors} = require('celebrate');
- app.use(routes);
-app.use(errors());
+const routes = require('./routes')
+
+const app = express()
+
+app.use(cors())
+
+app.use(express.json());
+app.use(routes);
 
 
 app.listen(process.env.PORT || 3000);
 
 
-
-
-
+console.log("NodeJS Server Online on port: 5555")
 
 
 
