0ae211a599f829530890bbad49bf5f6475fb11f5
Wesleson Souza Silva <31991735+weslleson22@users.noreply.github.com>
2020-05-18 22:31:40 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -14,13 +14,15 @@ app.use(routes);
 app.use(errors());
 
 
+
+
 const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .set('src', path.join(__dirname, 'src'))
+  .set('src engine', 'ejs')
+  .get('/', (req, res) => res.render('server'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 
